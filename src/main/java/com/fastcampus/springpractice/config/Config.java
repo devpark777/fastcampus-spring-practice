65c37ca8c2f65ef2cb9f0069df46507b28ae1995
devpark777 <yupark777@gmail.com>
2022-02-11 18:25:45 +0900

    spring으로 적용함

---
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.fastcampus.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus.springpractice")
+@Configuration //spring 전용 configuration 이 됨
+public class Config {
+}
